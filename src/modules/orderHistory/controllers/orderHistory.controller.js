3ba28f0d408b6dbe1d524cdcd4f2fe48ce097773
Nalek007895 <6206021622020@fitm.kmutnb.ac.th>
2022-12-22 00:00:26 +0700

    bug fix

--- src/modules/orderHistory/controllers/orderHistory.controller.js
+++ src/modules/orderHistory/controllers/orderHistory.controller.js
@@ -3,7 +3,7 @@ import OrderHistoryService from '../services/orderHistory.service.js'
 //import OrderItemService from '../../orderItem/services/orderItem.service.js'
 
 const OrderHistoryController = {
-    getOrderHistory: async (req, res) =>{
+    getOrderHistory: async (req, res) => {
         const orderHistory = await OrderHistoryService.getAll()
 
         res.status(200).json({
@@ -21,36 +21,43 @@ const OrderHistoryController = {
         })
     },
     getOrderHistoryByIdRepeat: async (req, res) => {
-        const { id } = req.params
-        const orderHistory = await OrderHistoryService.getRepeat(id)
+        try {
+            const { id } = req.params
+            const orderHistory = await OrderHistoryService.getRepeat(id)
 
-        res.status(200).json({
-            success: true,
-            data: orderHistory
-        })
+            res.status(200).json({
+                success: true,
+                data: orderHistory
+            })
+        } catch (error) {
+            res.status(200).json({
+                success: false,
+                data: error
+            })
+            console.log(error)
+        }
+
+
+        
     },
 
     createOrderHistory: async (req, res) => {
         ///const id = req.body.id
-        const { customer, item , orderQuantitytotal, orderPricetotal, orderRepeattype, dateOfbuy, deliveryStatus, idPromotion, payment } = req.body
-        
-        
-        const {sumPrice,sumQuantity, repeatType} = OrderHistoryService.getCumulativeAmount(item) 
-        // const orderQuantityTotal = OrderHistoryService.getQuantityCal(item) 
-        // const orderRepeatType = OrderHistoryService.getRepeatType(item)
-        //const order = await OrderItemService.getOne( idOrder )
-        const created = await OrderHistoryService.create({ customer, item , orderPriceTotal: sumPrice , orderQuantityTotal: sumQuantity, orderRepeatType: repeatType,  dateOfbuy, deliveryStatus, idPromotion, payment })
-    
+        const { customer, item, orderQuantitytotal, orderPricetotal, orderRepeattype, dateOfbuy, deliveryStatus, idPromotion, payment } = req.body
+
+        const { sumPrice, sumQuantity, repeatType } = OrderHistoryService.getCumulativeAmount(item)
+        const created = await OrderHistoryService.create({ customer, item, orderPriceTotal: sumPrice, orderQuantityTotal: sumQuantity, orderRepeatType: repeatType, dateOfbuy, deliveryStatus, idPromotion, payment })
+
         res.status(201).json({
             success: true,
             data: created
         })
     },
-    updateOrderHistory: async (req, res) =>{
+    updateOrderHistory: async (req, res) => {
         const { id } = req.params
         const { customer, order, orderPriceTotal, dateOfbuy, deliveryStatus, idPromotion, payment } = req.body
         const updated = await OrderHistoryService.updateOne(id, { customer, order, orderPriceTotal, dateOfbuy, deliveryStatus, idPromotion, payment })
-        
+
         res.status(200).json({
             success: true,
             data: updated
@@ -67,3 +74,7 @@ const OrderHistoryController = {
     },
 }
 export default OrderHistoryController
+
+// const orderQuantityTotal = OrderHistoryService.getQuantityCal(item)
+        // const orderRepeatType = OrderHistoryService.getRepeatType(item)
+        //const order = await OrderItemService.getOne( idOrder )
--- src/modules/orderHistory/services/orderHistory.service.js
+++ src/modules/orderHistory/services/orderHistory.service.js
@@ -1,4 +1,6 @@
 import OrderHistoryModel from '../models/orderHistory.schema.js'
+import CustomerModel from '../../customer/models/customer.schema.js'
+//import CustomerService from '../../customer/services/customer.service.js'
 
 const OrderHistoryService = {
     create: (payload) => {
@@ -32,65 +34,81 @@ const OrderHistoryService = {
         return OrderHistoryModel.findOne({ _id: id })
     },
     getRepeat: async (id) => {
+      let _id = id
+      let customerId = await OrderHistoryModel.find( {'customer.idCustomer': _id} )
+      let customerModel = await CustomerModel.findOne({ _id })
 
-        const customerId = await OrderHistoryModel.find({ 'customer.idCustomer': id })
-        let count = customerId.length
-        //customer 
-        let cusTomer = customerId[0].customer
-        //orderitem
-        let orderItem = [] //[customerId[0].item[0].nameItem],[0] [''],[0]
-        for (let i = 0; i < customerId.length; i++) {
-            for (let j = 0; j < customerId[i].item.length; j++) {
-                orderItem.push(customerId[i].item[j])
-                //allItem.push[customerId[i].item[j].nameItem]
-                //[customerId[i].item[j].quantityItem]
-            }
+      //return customerId.length
+      if (customerId.length > 0) {
+        if ( (customerId[0].customer).idCustomer === _id ) {
+          let count = customerId.length
+          //customer 
+          let cusTomer = customerId[0].customer
+          //orderitem
+          let orderItem = [] //[customerId[0].item[0].nameItem],[0] [''],[0]ss
+          for (let i = 0; i < customerId.length; i++) {
+              for (let j = 0; j < customerId[i].item.length; j++) {
+                  customerId[i].item[j].dateOfbuy = customerId[i].dateOfbuy
+                  customerId[i].item[j].payment = customerId[i].payment
+                  orderItem.push(
+                      customerId[i].item[j]
+                  )
+              }
+          }
+          let allItemRepeat = []
+          let allTypeRepeat = []
+          for (let j = 0; j < orderItem.length; j++) {
+              //repeat orderitem
+              if(orderItem[j].nameItem){
+                  const findsort = allItemRepeat.findIndex((items) => items.nameItem === orderItem[j].nameItem) 
+              
+                  if(findsort >= 0) {
+                      allItemRepeat[findsort].qty += orderItem[j].quantityItem
+                      allItemRepeat[findsort].totalItem += orderItem[j].priceItem * orderItem[j].quantityItem
+                  }
+  
+                  else {
+                      allItemRepeat.push({
+                          nameItem: orderItem[j].nameItem,
+                          qty: orderItem[j].quantityItem,
+                          totalItem: orderItem[j].priceItem * orderItem[j].quantityItem
+                      })
+                  }
+              }
+              //repeat typeitem
+              if(orderItem[j].typeItem){
+                  const findsort = allTypeRepeat.findIndex((items) => items.typeItem === orderItem[j].typeItem) 
+                  
+                  if(findsort >= 0) {
+                      allTypeRepeat[findsort].qty += orderItem[j].quantityItem
+                      allTypeRepeat[findsort].totalType += orderItem[j].priceItem * orderItem[j].quantityItem
+                  }
+  
+                  else {
+                      allTypeRepeat.push({
+                          typeItem: orderItem[j].typeItem,
+                          qty: orderItem[j].quantityItem,
+                          totalType: orderItem[j].priceItem * orderItem[j].quantityItem
+                      })
+                  }
+              }
+          } 
+          //จำนวนสินค้าทั้งหมดที่เคยซื้อ ยอดซื้อทั้งหมดที่เคยซื้อ
+          let totalquantityItem = 0
+          let totalpriceItem = 0
+          for (let i = 0; i < customerId.length; i++) {
+              totalquantityItem += customerId[i].orderQuantityTotal
+              totalpriceItem += customerId[i].orderPriceTotal
+          }
+          return { count, cusTomer, totalquantityItem, totalpriceItem, allItemRepeat, allTypeRepeat, orderItem,  }
         }
-        //let B = [...(new Set(A.map(({ nameItem }) => nameItem)))];
-        //let BB = []
-        let allItemRepeat = []
-        let allTypeRepeat = []
-        for (let j = 0; j < orderItem.length; j++) {
-            //repeat orderitem
-            if(orderItem[j].nameItem){
-                const findsort = allItemRepeat.findIndex((items) => items.nameItem === orderItem[j].nameItem) 
-            
-                if(findsort >= 0) {
-                    allItemRepeat[findsort].qty += orderItem[j].quantityItem
-                }
+      } else {
+      if (customerModel._id == _id) {
+        return customerModel
+      }
+      return null
+    }
 
-                else {
-                    allItemRepeat.push({
-                        nameItem: orderItem[j].nameItem,
-                        qty: orderItem[j].quantityItem
-                    })
-                }
-            }
-            //repeat typeitem
-            if(orderItem[j].typeItem){
-                const findsort = allTypeRepeat.findIndex((items) => items.typeItem === orderItem[j].typeItem) 
-                
-                if(findsort >= 0) {
-                    allTypeRepeat[findsort].qty += orderItem[j].quantityItem
-                }
-
-                else {
-                    allTypeRepeat.push({
-                        typeItem: orderItem[j].typeItem,
-                        qty: orderItem[j].quantityItem
-                    })
-                }
-            }
-        } 
-        //จำนวนสินค้าทั้งหมดที่เคยซื้อ ยอดซื้อทั้งหมดที่เคยซื้อ
-        let totalquantityItem = 0
-        let totalpriceItem = 0
-        for (let i = 0; i < customerId.length; i++) {
-            totalquantityItem += customerId[i].orderQuantityTotal
-            totalpriceItem += customerId[i].orderPriceTotal
-        }
-
-        return { count, cusTomer, totalquantityItem, totalpriceItem, allItemRepeat, allTypeRepeat, orderItem,  }
     },
     updateOne: (id, payload) => {
         return OrderHistoryModel.findOneAndUpdate({ _id: id }, { $set: payload })
@@ -150,3 +168,6 @@ export default OrderHistoryService
         //     count += 1
         // }
         //const CCC = customerId.dateOfbuy
+
+        //let B = [...(new Set(A.map(({ nameItem }) => nameItem)))];
+        //let BB = []
